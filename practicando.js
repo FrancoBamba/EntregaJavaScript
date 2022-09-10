b2394557cbb2903ee7af1026c9427e3c3a8efe55
FrancoBamba <105562809+FrancoBamba@users.noreply.github.com>
2022-09-10 17:09:41 -0300

    Delete practicando.js

--- practicando.js
+++
@@ -1,3 +0,0 @@
-// Me gustaria hacer un contador de promedio nde edades de personas, el HTML mas o menos lo tengo, faltaria agregarle cosas y mas adelante el CSS,
-// con eso no hay problema, lo que se me complico y por eso no lo arme es el JS, ya que deberia vincular cosas del HTML con el JS 
-// y por el momento no sabria como hacerlo.
